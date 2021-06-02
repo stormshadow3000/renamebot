17d60658560e3ca765572b01117e4ec846b65dc1
stormshadow3000 <83730643+stormshadow3000@users.noreply.github.com>
2021-06-02 17:41:29 +0530

    Update chat_base.py

--- chat_base.py
+++ chat_base.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Anonymous
-
-# the logging things
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
